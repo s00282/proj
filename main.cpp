492aa3cd6cade253f70830d4f6947cd9eedb2cb3
s00282 <s002825269@mail.com.tw>
2015-06-20 11:50:46 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include<stdio.h>
 int main(){
-	printf("test");
+	printf("test1234567890-");
 	return 0;
 }
+//git init 初始 第一次用就好
+//git add main.cpp 加入追蹤
+//git diff 查看文件修改記錄
+//git status
+
+//git config --global user.email "123@mail.com"
+//git config --global user.name "123"
+//git log 看修改紀錄 mail,time,user name
+//git commit -m 'main.cpp' 提交
+
+//git config --global alias.st status  設定熱建
+//git config -l //列出所有狀態資料
+
+//github
+//註冊帳號，mail and user 都要一樣
+//git remote add origin https://github.com/s00282/proj.git  //設定位置
+//git push -u origin master //上傳檔案，上船前記得commit
